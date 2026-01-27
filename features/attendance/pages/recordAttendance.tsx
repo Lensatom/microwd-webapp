29c01ff0da36ef8a8c01cf1d5988d88093056f8e
lensatom <ayomidetommiwa@gmail.com>
2026-01-27 17:08:46 +0100

    refactor: streamline error handling and improve readability in RecordAttendance component

--- features/attendance/pages/recordAttendance.tsx
+++ features/attendance/pages/recordAttendance.tsx
@@ -1,26 +1,22 @@
 import { GET } from '@/shared/config/api/crud'
-import { redirect } from 'next/navigation'
 import DataSharePrompt from '../components/partials/dataSharePrompt'
+import { redirect } from 'next/navigation'
 
 async function RecordAttendance({
   eventId
 }: {eventId: string}) {
-  try {
-    const response = await GET({
-      route: `/events/${eventId}`,
-      isServer: true
-    })
+  const response = await GET({
+    route: `/events/${eventId}`,
+    isServer: true
+  })
   
-    const eventDetails = response.event
+  const eventDetails = response.event
 
-    if (eventDetails.hasFilled) {
-      redirect(`/attendance/${eventId}/record/success`)
-    }
-    
-    return <DataSharePrompt eventDetails={eventDetails} />
-  } catch (error) {
-    return <>Error</>
+  if (eventDetails.hasFilled) {
+    redirect(`/attendance/${eventId}/record/success`)
   }
+  
+  return <DataSharePrompt eventDetails={eventDetails} />
 }
 
 export default RecordAttendance
