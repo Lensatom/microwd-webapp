be5e2d3b20fdbb25b7ed067eb4e335cd01a63a3c
lensatom <ayomidetommiwa@gmail.com>
2026-03-27 10:45:20 +0100

    fix: remove unused Loader import and simplify icon usage in DownloadAttendance component

--- features/events/components/downloadAttendance.tsx
+++ features/events/components/downloadAttendance.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Button, Loader } from "@/shared/components/ui";
+import { Button } from "@/shared/components/ui";
 import { ArrowDownToLine } from "lucide-react";
 import useDownloadCsv from "../hooks/useDownloadCsv";
 import { IEvent } from "../types";
@@ -12,11 +12,9 @@ export default function DownloadAttendance({
 
   const { isLoading, handleDownloadCsv } = useDownloadCsv({ event });
 
-  const Icon = isLoading ? Loader : ArrowDownToLine;
-
   return (
     <Button disabled={isLoading || disabled} onClick={handleDownloadCsv} variant="outline" size="sm" className="flex items-center gap-2 mt-2 text-sm">
-      <Icon /> {isLoading ? "Downloading..." : "Download CSV"}
+      <ArrowDownToLine /> {isLoading ? "Downloading..." : "Download CSV"}
     </Button>
   )
 }
